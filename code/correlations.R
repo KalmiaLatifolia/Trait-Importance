7197d486f81c78a0c1a7929feed1386ccf39dcae
KalmiaLatifolia <lauraberman11@gmail.com>
2026-04-03 14:22:32 -0500

    update - NFPD, fabian's new variables

--- code/correlations.R
+++ code/correlations.R
@@ -1,6 +1,10 @@
 
+# Partial Protocol - Trait importance manuscript
+# create correlation matrix (Figure 2)
+
 # correlations
-# 18 December 2025
+# created 18 December 2025
+# updated 3 April 2026
 
 library(ggplot2)
 library(tidyverse)
@@ -11,75 +15,70 @@ library(ggcorrplot)
 
 setwd("/Users/lauraberman/Library/CloudStorage/OneDrive-NationalUniversityofSingapore/Documents/Wisconsin/Townsend Lab/Trait importance/TraitImportance_GIT")
 
-# read full bird data ----------------------------------------------------------
+# load dataset -----------------------------------------------------------------
 
-siteDetections <- readRDS("data/siteDetections_foliarTraits_BioCube_20250522.rds")
+siteDetections_foliarTraits_BioCube <- readRDS("data/siteDetections_foliarTraits_BioCube_20260320.rds")
 
-# remove bad vars
-siteDetections <- siteDetections %>% select(-gsp, -GHM_bu, -GHM_en, -NTL_2020)
+# set up variable groups -------------------------------------------------------
+names(siteDetections_foliarTraits_BioCube)
+species = siteDetections_foliarTraits_BioCube[4:97]
+spatVars  = siteDetections_foliarTraits_BioCube[98:234]
 
-# variable groups --------------------------------------------------------------
+# NFPD function ----------------------------------------------------------------
+FPD <- function(species_col) {species_col / max(species_col)}
+meanFPD <- function(species_col) {mean(FPD(species_col)[FPD(species_col)>0])}
+NFPD <- function(species_col) {(FPD(species_col))^(log(0.5)/log(meanFPD(species_col)))}
 
-species = siteDetections[5:96]
-spatVars  = siteDetections[97:178]
 
-traits  = siteDetections[97:108]
-anthr = siteDetections[109:120]
-climate = siteDetections[121:147]
-fxn = siteDetections[c(148:150, 175:178)]
-pheno = siteDetections[151:160]
-topo = siteDetections[161:174]
-
-traitsnFxn = siteDetections[c(97:108, 148:150, 175:178)]
-
-# do a correlation -------------------------------------------------------------
-
-sp <- names(species[1])
-trait <- names(traits[1])
-
-ggplot(siteDetections, aes(x=get(trait), y=NFPD(get(sp)))) +
-  geom_point() +
-  geom_smooth(method = lm) +
-  theme_minimal()
-
-# spearman correlation for non-linear monotonic relationships
+# spearman correlation for non-linear monotonic relationships ------------------
 spTraitCors <- as.data.frame(cor(NFPD(species), spatVars, method = "spearman"))
 
-# tidy variable labels
-
-var_labels <- read.csv("data/var_labels.csv")
-
-# publication worthy correlation matrix ----------------------------------------
-
-# pivot longer
+# pivot longer -----------------------------------------------------------------
 cors_df <- as.data.frame(spTraitCors) %>%
   rownames_to_column("species") %>%           
   pivot_longer(-species, names_to = "Variable", values_to = "rho") 
 
-# add variable labels
+# hclust rows and columns ------------------------------------------------------
+
+row_hc <- hclust(as.dist(1 - cor(t(spTraitCors), method = "spearman")), 
+                 method = "centroid")$order
+
+col_hc <- hclust(as.dist(1 - cor(spTraitCors, method = "spearman")),
+                 method = "centroid")$order
+
+# Tidy variable labels ---------------------------------------------------------
+
+# load tidy names
+tidy <- read_excel("/Users/lauraberman/Library/CloudStorage/OneDrive-NationalUniversityofSingapore/Documents/Wisconsin/Townsend Lab/Trait importance/TableS1_Biocube_var_description.xlsx",
+                   range = cell_cols(1:4))
+
+# add variable labels to correlation dataframe 
 cors_df <- cors_df %>%
-  left_join(var_labels, by = "Variable") %>%
-  mutate(Label = factor(Label, levels = var_labels$Label))  # preserve desired axis order
+  left_join(tidy, by = "Variable") %>%
+  mutate(Label = factor(Label, levels = tidy$Label))  # preserve desired axis order
 
-# hclust rows and columns
-row_ord <- hclust(as.dist(1 - cor(t(spTraitCors), method = "spearman")), 
-                  method = "average")$order
+# data exploration - look at specific cor values
+cors_df %>%
+  filter(species == "Mountain Bluebird", Label == "Canopy height") %>%
+  pull(rho)
 
-col_ord <- hclust(as.dist(1 - cor(spTraitCors, method = "spearman")),
-                  method = "average")$order
-
-# plot it
+# plot it ----------------------------------------------------------------------
 ggplot(cors_df,
-       aes(x = factor(Variable, levels = colnames(spTraitCors)[col_ord]),
-           y = factor(species, levels = rownames(spTraitCors)[row_ord]),
+       aes(x = factor(Variable, levels = colnames(spTraitCors)[col_hc]),
+           y = factor(species, levels = rownames(spTraitCors)[row_hc]),
            fill = rho)) +
   geom_tile() +
-  scale_fill_gradient2(limits = c(-0.8, 0.8), midpoint = 0, name = "Spearman’s ρ") +
+  scale_fill_gradient2(limits = c(-0.85, 0.8), midpoint = 0, name = "Spearman’s ρ") +
   theme_minimal() +
   theme(axis.text.x = element_text(size = 6, angle = 45, hjust = 1,
-                                   colour = var_labels$lab_color[match(colnames(spTraitCors)[col_ord], var_labels$Variable)]),
+                                   colour = tidy$labelColor[match(colnames(spTraitCors)[col_hc], tidy$Variable)]),
         axis.text.y = element_text(size = 6)) +
   xlab("") +
   ylab("") +
-  scale_x_discrete(labels = var_labels$Label[match(colnames(spTraitCors)[col_ord], var_labels$Variable)])
+  scale_x_discrete(labels = tidy$Label[match(colnames(spTraitCors)[col_hc], tidy$Variable)])
+
+# save it
+ggsave("figures/Correlation_Matrix1.pdf", height=10, width=15)
+
+
 
